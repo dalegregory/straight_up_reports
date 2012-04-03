b2126ce5f75324cb80dc7dfdac9e4d589061d8eb
dalegregory <dale.gregory@me.com>
2012-04-03 10:06:03 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StraightUpReports::Application.config.secret_token = '203d49a1a66a0cb99ecde3000108384d1a293657f51a0fc8675009992474d81109b9bfd9f8e74bf346280b6d640d923f1574a3c1a1576df89c9c561b7723c06b'
